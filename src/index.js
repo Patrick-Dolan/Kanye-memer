11403f76d0c382f25d9a1b3dce26e46781edfec6
Patrick Dolan <dolanp1992@gmail.com>
2022-01-08 19:35:50 -0800

    add GiphyService to UI

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import KanyeService from "./js/KanyeService";
+import GiphyService from "./js/GiphyService";
 
 $(document).ready(() => {
   $("#generateMeme").click(() => {
+    let keyword = "cat";
     KanyeService.getQuote()
       .then((response) => {
         if (response.quote) {
           $("#showQuote").text(`${response.quote}`);
+        } else {
+          $("#showError").text(response.message);
+        }
+      });
+    GiphyService.getGif(keyword)
+      .then((response) => {
+        if (response.data) {
+          $("#showGif").html(`<img src="${response.data.images.original.url}">`);
         } else {
           $("#showError").text(response.message);
         }
