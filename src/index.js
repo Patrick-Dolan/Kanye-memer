0fa2bb1410d09cd5dbf2a55466d20fc28817fbcb
Patrick Dolan <dolanp1992@gmail.com>
2022-01-08 20:21:14 -0800

    remove gif search based on longest word

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import KanyeService from "./js/KanyeService";
 import GiphyService from "./js/GiphyService";
+// import getLongestWord from "./js/searchQuote";
 
 $(document).ready(() => {
   $("#generateMeme").click(() => {
-    let keyword = "cat";
     KanyeService.getQuote()
       .then((response) => {
         if (response.quote) {
@@ -15,14 +15,16 @@ $(document).ready(() => {
         } else {
           $("#showError").text(response.message);
         }
-      });
-    GiphyService.getGif(keyword)
-      .then((response) => {
-        if (response.data) {
-          $("#showGif").html(`<img src="${response.data.images.original.url}">`);
-        } else {
-          $("#showError").text(response.message);
-        }
+      })
+      .then(() => {
+        GiphyService.getGif("Kanye West")
+          .then((response) => {
+            if (response.data) {
+              $("#showGif").html(`<img src="${response.data.images.original.url}">`);
+            } else {
+              $("#showError").text(response.message);
+            }
+          });
       });
   });
 });
