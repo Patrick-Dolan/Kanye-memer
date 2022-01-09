42f608c80eb1b064435254911c0b6285e2a8c397
Patrick Dolan <dolanp1992@gmail.com>
2022-01-08 17:22:05 -0800

    add KanyeService API fetch for quotes

---
+++ src/js/KanyeService.js
@@ -0,0 +1,14 @@
+export default class KanyeService {
+  static getQuote(keyword) {
+    return fetch(`https://api.kanye.rest/`)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch((error) => {
+        return error;
+      })
+  }
+}
