9521027a7306ca9d9f2ed8055a7790044799725d
Patrick Dolan <dolanp1992@gmail.com>
2022-01-08 17:35:24 -0800

    add KanyeService import and click functionality to add quote to html

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import $ from "jquery";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import KanyeService from "./js/KanyeService";
+
+$(document).ready(() => {
+  $("#generateMeme").click(() => {
+    KanyeService.getQuote()
+      .then((response) => {
+        if (response.quote) {
+          $("#showQuote").text(`${response.quote}`);
+        } else {
+          $("#showError").text(response.message);
+        }
+      });
+  });
+});
